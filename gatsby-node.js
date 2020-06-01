786f1a6da80c15e42f9a52e140a91c12e4f4cf8f
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-06-01 15:24:44 +0200

    still debugging

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const module = typeof window !== `undefined` ? require('module') : null;
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 	if (stage === 'build-html') {
 		actions.setWebpackConfig({
